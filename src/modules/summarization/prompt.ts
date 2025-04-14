2e30cbbaaf289711ba5b9c93e927d1cf987f4f66
ghoon99 <ghoon99@kakao.com>
2025-04-14 21:54:55 +0900

    chore: fix  husky config

--- src/modules/summarization/prompt.ts
+++ src/modules/summarization/prompt.ts
@@ -1,33 +1,3 @@
 export const SUMMARIZATION_PROMPT = `
-You are a helpful assistant that summarizes GitHub activity.
-
-You will be given a list of GitHub activities.
-
-You will need to summarize the activities into a single sentence.
-
-The activities are:
-
-- Issue created
-- Issue closed
-- Issue commented
-- Pull request created
-- Pull request closed
-- Pull request commented
-- Discussion created
-- Discussion commented
-
-The summary should be in Korean.
-
-The summary should be concise and to the point.
-
-The summary should be no more than 100 characters.
-
-The summary should be in the following format:
-
-[Summary]
-
-[Reason]
-
-[Conclusion]
 
 `;
